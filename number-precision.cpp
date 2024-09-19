245b3c5d7339be8f892730f6abb3382458ba0cdf
strange <38800238+rivasjoaquin02@users.noreply.github.com>
2024-09-19 15:20:07 -0400

    num precision

---
+++ number-precision.cpp
@@ -0,0 +1,13 @@
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    std::cout.precision(n + 1);
+    std::cout << M_PI;
+
+    return 0;
+}
---
+++ number-precision.py
@@ -0,0 +1,6 @@
+import math
+
+n = int(input())
+
+print (f'{math.pi:.{n}f}')
+print (f'{round(math.pi, n)}')
